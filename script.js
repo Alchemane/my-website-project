3ff1213ce35c5904f8acc625efdc6f9e197ddc1b
Alchemane <kevsaxton7@gmail.com>
2023-09-15 23:25:32 +0100

    added all icons 2 and reworked positioning relative to the profile picture

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// Interactive functionalities go here
+document.addEventListener("DOMContentLoaded", function() {
+    const numberOfIcons = 11; // Total number of social icons
+    const radius = 200; // Radius in pixels
+    const angleStep = 360 / numberOfIcons;
+    const centerWrapper = document.getElementById("center-wrapper");
+  
+    // Array of social icon IDs
+    const iconIds = [
+      "discord-icon", "steam-icon", "instagram-icon", "reddit-icon",
+      "twitter-icon", "github-icon", "youtube-icon", "anime-planet-icon",
+      "tiktok-icon", "spotify-icon", "soundcloud-icon"
+    ];
+  
+    iconIds.forEach((id, i) => {
+      const icon = document.getElementById(id);
+      const angle = angleStep * i;
+      const x = radius * Math.cos(angle * (Math.PI / 180));
+      const y = radius * Math.sin(angle * (Math.PI / 180));
+  
+      // Set the position of each icon
+      icon.style.left = `calc(50% + ${x}px)`;
+      icon.style.top = `calc(50% + ${y}px)`;
+    });
+  });
