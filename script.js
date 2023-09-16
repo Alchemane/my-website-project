06cd24ecba344a4c9e8d92c94e08b86a03a8c34f
Alchemane <kevsaxton7@gmail.com>
2023-09-16 17:04:26 +0100

    added debugging lines

--- script.js
+++ script.js
@@ -2,24 +2,44 @@ document.addEventListener("DOMContentLoaded", function() {
     const numberOfIcons = 11;
     const radius = 200;
     const angleStep = 360 / numberOfIcons;
-  
+
     const iconIds = [
-      "discord-icon", "steam-icon", "instagram-icon", "reddit-icon",
-      "twitter-icon", "github-icon", "youtube-icon", "anime-planet-icon",
-      "tiktok-icon", "spotify-icon", "soundcloud-icon"
+        "discord-icon", "steam-icon", "instagram-icon", "reddit-icon",
+        "twitter-icon", "github-icon", "youtube-icon", "anime-planet-icon",
+        "tiktok-icon", "spotify-icon", "soundcloud-icon"
     ];
-  
+
+    const profilePic = document.getElementById("profile-pic");
+    const profilePicRect = profilePic.getBoundingClientRect();
+    const profilePicCenterX = profilePicRect.left + profilePicRect.width / 2;
+    const profilePicCenterY = profilePicRect.top + profilePicRect.height / 2;
+
+    console.log(`Profile Picture Center: ${profilePicCenterX}px, ${profilePicCenterY}px`);
+
+    
     iconIds.forEach((id, i) => {
-      const icon = document.getElementById(id);
-      const angle = angleStep * i;
-      const x = radius * Math.cos(angle * (Math.PI / 180));
-      const y = radius * Math.sin(angle * (Math.PI / 180));
-  
-      // Set the position of each icon
-      icon.style.left = `calc(50% + ${x}px)`;
-      icon.style.top = `calc(50% + ${y}px)`;
-  
-      // Set the initial rotation for each icon
-      icon.style.transform = `rotate(${angle}deg) translate(${radius}px) rotate(-${angle}deg)`;
+        const icon = document.getElementById(id);
+        const iconRect = icon.getBoundingClientRect();
+        const iconCenterX = iconRect.left + iconRect.width / 2;
+        const iconCenterY = iconRect.top + iconRect.height / 2;
+
+        console.log(`Initial ${id} Center: ${iconCenterX}px, ${iconCenterY}px`);
+
+
+        const transformOriginX = profilePicCenterX - iconCenterX;
+        const transformOriginY = profilePicCenterY - iconCenterY;
+
+        icon.style.transformOrigin = `${transformOriginX}px ${transformOriginY}px`;
+
+        console.log(`Setting transform-origin for ${id} to ${transformOriginX}px ${transformOriginY}px`);
+
+        const angle = angleStep * i;
+        const x = radius * Math.cos(angle * (Math.PI / 180));
+        const y = radius * Math.sin(angle * (Math.PI / 180));
+
+        icon.style.left = `calc(50% + ${x}px)`;
+        icon.style.top = `calc(50% + ${y}px)`;
+        icon.style.transform = `rotate(${angle}deg) translate(${radius}px) rotate(-${angle}deg)`;
+        console.log(`Applied transform-origin for ${id}: ${window.getComputedStyle(icon).transformOrigin}`);
     });
-  });
+});
