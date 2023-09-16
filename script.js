0ac1702bcf1b995f14edd0ff29431309d5ad3c7e
Alchemane <kevsaxton7@gmail.com>
2023-09-16 21:53:15 +0100

    unfinished work on icon rotation

--- script.js
+++ script.js
@@ -8,31 +8,9 @@ document.addEventListener("DOMContentLoaded", function() {
         "twitter-icon", "github-icon", "youtube-icon", "anime-planet-icon",
         "tiktok-icon", "spotify-icon", "soundcloud-icon"
     ];
-
-    const profilePic = document.getElementById("profile-pic");
-    const profilePicRect = profilePic.getBoundingClientRect();
-    const profilePicCenterX = profilePicRect.left + profilePicRect.width / 2;
-    const profilePicCenterY = profilePicRect.top + profilePicRect.height / 2;
-
-    console.log(`Profile Picture Center: ${profilePicCenterX}px, ${profilePicCenterY}px`);
-
     
     iconIds.forEach((id, i) => {
         const icon = document.getElementById(id);
-        const iconRect = icon.getBoundingClientRect();
-        const iconCenterX = iconRect.left + iconRect.width / 2;
-        const iconCenterY = iconRect.top + iconRect.height / 2;
-
-        console.log(`Initial ${id} Center: ${iconCenterX}px, ${iconCenterY}px`);
-
-
-        const transformOriginX = profilePicCenterX - iconCenterX;
-        const transformOriginY = profilePicCenterY - iconCenterY;
-
-        icon.style.transformOrigin = `${transformOriginX}px ${transformOriginY}px`;
-
-        console.log(`Setting transform-origin for ${id} to ${transformOriginX}px ${transformOriginY}px`);
-
         const angle = angleStep * i;
         const x = radius * Math.cos(angle * (Math.PI / 180));
         const y = radius * Math.sin(angle * (Math.PI / 180));
@@ -40,6 +18,6 @@ document.addEventListener("DOMContentLoaded", function() {
         icon.style.left = `calc(50% + ${x}px)`;
         icon.style.top = `calc(50% + ${y}px)`;
         icon.style.transform = `rotate(${angle}deg) translate(${radius}px) rotate(-${angle}deg)`;
-        console.log(`Applied transform-origin for ${id}: ${window.getComputedStyle(icon).transformOrigin}`);
+        
     });
 });
