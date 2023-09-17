c8488d5e2f879d7db58a735425088519c9574441
Alchemane <kevsaxton7@gmail.com>
2023-09-17 18:25:50 +0100

    tweaked code, current iteration only accommodates 1 icon for testing

--- script.js
+++ script.js
@@ -6,12 +6,19 @@ document.addEventListener("DOMContentLoaded", function() {
     const iconIds = [
         "soundcloud-icon"
     ];
+
+    const profilePicture = document.getElementById('profile-picture');
+    const profileRect = profilePicture.getBoundingClientRect();
+    const centerX = profileRect.left + profileRect.width / 2;
+    const centerY = profileRect.top + profileRect.height / 2;
     
     iconIds.forEach((id, i) => {
         const icon = document.getElementById(id);
         const angle = angleStep * i;
         const x = radius * Math.cos(angle * (Math.PI / 180));
         const y = radius * Math.sin(angle * (Math.PI / 180));
+
+        icon.style.transformOrigin = `${centerX}px ${centerY}px`;
         
         icon.style.left = `calc(50% + ${x}px)`;
         icon.style.top = `calc(50% + ${y}px)`;
