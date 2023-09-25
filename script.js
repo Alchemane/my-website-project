994f0c771999f92412cd05b0052f05fba93ee807
Alchemane <kevsaxton7@gmail.com>
2023-09-25 15:19:10 +0100

    added nav buttons and more work on about page

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function scrollToSection(sectionId) {
-    const section = document.getElementById(sectionId);
-    section.scrollIntoView({ behavior: 'smooth' });
-  }
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('.nav-link');
+  
+    navLinks.forEach(link => {
+      link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const sectionId = this.getAttribute('data-section');
+        const sectionElement = document.getElementById(sectionId);
+  
+        window.scrollTo({
+          top: sectionElement.offsetTop,
+          behavior: 'smooth'
+        });
+      });
+    });
+  });
