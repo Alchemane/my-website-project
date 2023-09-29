0fe465806a684bc5d214b4cac6f0eee586e502a6
Alchemane <kevsaxton7@gmail.com>
2023-09-29 01:51:30 +0100

    added social background and improved title texts. added glitch effect to all text needed by passing any text needed to glitch function as arguments in array parameter

--- script.js
+++ script.js
@@ -14,32 +14,38 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
-    //glitch animation
-  const homeTitle = document.getElementById("home-title");
-  const originalText = homeTitle.textContent;
-  let isGlitching = false;
-
-  function randomChar() {
-    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%^&*()";
-    return chars[Math.floor(Math.random() * chars.length)];
+  function applyGlitchEffect(elementIds) {
+    elementIds.forEach(elementId => {
+      const element = document.getElementById(elementId);
+      const originalText = element.textContent;
+      let isGlitching = false;
+    
+      function randomChar() {
+        const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%^&*()";
+        return chars[Math.floor(Math.random() * chars.length)];
+      }
+    
+      function glitchText() {
+        if (isGlitching) return;
+        isGlitching = true;
+    
+        let glitchedText = "";
+        for (let i = 0; i < originalText.length; i++) {
+          glitchedText += randomChar();
+        }
+    
+        element.textContent = glitchedText;
+    
+        setTimeout(() => {
+          element.textContent = originalText;
+          isGlitching = false;
+        }, 100); // how long the glitch text appears for
+      }
+    
+      setInterval(glitchText, 1500); // how often the glitch text appears
+    });
   }
 
-  function glitchText() {
-    if (isGlitching) return;
-    isGlitching = true;
-
-    let glitchedText = "";
-    for (let i = 0; i < originalText.length; i++) {
-      glitchedText += randomChar();
-    }
-
-    homeTitle.textContent = glitchedText;
-
-    setTimeout(() => {
-      homeTitle.textContent = originalText;
-      isGlitching = false;
-    }, 100);
-  }
-
-  setInterval(glitchText, 3000);
+  //pass any number of text elements as an array
+  applyGlitchEffect(["home-title", "social-title", "about-title"]);
 });
