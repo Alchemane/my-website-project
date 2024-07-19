5271f78de1ea8cfb4502cad70aa049cd484d8e0a
Alchemane <kevsaxton7@gmail.com>
2024-07-19 22:44:00 +0100

    major changes

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
       e.preventDefault();
     }
   });
+
   const navLinks = document.querySelectorAll('.nav-link');
 
   navLinks.forEach(link => {
@@ -24,33 +25,62 @@ document.addEventListener('DOMContentLoaded', function() {
       const element = document.getElementById(elementId);
       const originalText = element.textContent;
       let isGlitching = false;
-    
+
       function randomChar() {
         const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()_-[]{}/?+=!$~";
         return chars[Math.floor(Math.random() * chars.length)];
       }
-    
+
       function glitchText() {
         if (isGlitching) return;
         isGlitching = true;
-    
+
         let glitchedText = "";
         for (let i = 0; i < originalText.length; i++) {
           glitchedText += randomChar();
         }
-    
+
         element.textContent = glitchedText;
-    
+
         setTimeout(() => {
           element.textContent = originalText;
           isGlitching = false;
         }, 100); // how long the glitch text appears for
       }
-    
+
       setInterval(glitchText, 3000); // how often the glitch text appears
     });
   }
 
-  //pass any number of text elements as an array
-  applyGlitchEffect(["home-title", "social-title", "about-title", "about-content"]);
+  function typewriterEffect(elementIds, typingSpeed = 100, resetDelay = 2000) {
+    elementIds.forEach(elementId => {
+      const element = document.getElementById(elementId);
+      if (element) {
+        const text = element.textContent;
+        element.textContent = '';
+        let index = 0;
+
+        function type() {
+          if (index < text.length) {
+            element.textContent += text.charAt(index);
+            index++;
+            setTimeout(type, typingSpeed);
+          } else {
+            setTimeout(() => {
+              element.textContent = '';
+              index = 0;
+              setTimeout(type, typingSpeed);
+            }, resetDelay);
+          }
+        }
+
+        type();
+      }
+    });
+  }
+  applyGlitchEffect(['home-title', 'social-title', 'about-title']);
+
+  typewriterEffect(['sub-home-title', 'sub-social-title', 'sub-about-title']);
+
+  typewriterEffect(['about-content'], 50, 10000);
 });
